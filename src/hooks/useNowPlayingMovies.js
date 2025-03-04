6a8134c30ecbe60e3d6c3ab04367ef2a69a4f841
Varad Kakatkar <varadk@valueaddsofttech.com>
2025-03-04 12:14:52 +0530

    wip tmdb api integration

--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -3,17 +3,28 @@ import { useDispatch } from "react-redux";
 import { addNowPlayingMovies } from "../utils/movieSlice";
 import { useEffect } from "react";
 
+
 const useNowPlayingMovies = () => {
   const dispatch = useDispatch();
   const getNowPlayingMovies = async () => {
     try {
-      const data = await fetch(
+       const data = await fetch(
         "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
         API_OPTIONS
       );
       const json = await data.json();
-      // console.log("json ", json.results);
       dispatch(addNowPlayingMovies(json.results));
+      console.log("json ", json.results); 
+    
+    
+     /*  fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
+        API_OPTIONS
+      )
+        .then((res) => res.json())
+        .then((res) => console.log(res))
+        .catch((err) => console.error(err)); */
+      
     } catch (error) {
       console.log("error---- ", error);
     }
